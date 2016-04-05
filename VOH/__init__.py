e523388cce691e3bacdb89b1888f9bdddb105eeb
aadhyagupta <Aadhya@wirelessprvnat-172-17-235-84.near.illinois.edu>
2016-04-05 13:42:42 -0500

    Adding file upload

--- VOH/__init__.py
+++ VOH/__init__.py
@@ -2,6 +2,8 @@ from pymongo import *
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
 client = MongoClient('104.131.185.191', 27017)
 db = client["225VOH"]
 
--- VOH/views.py
+++ VOH/views.py
@@ -1,12 +1,13 @@
 from flask import Flask, Blueprint
-from flask import render_template, request, session, jsonify
+from flask import render_template, request, session, jsonify, redirect
 from VOH import app
 from flask_socketio import *
 from authentication import *
+import os
 api = Blueprint('api', __name__)
 socketio = SocketIO(app)
 thread = None # keeping track of thread
-
+app.config['UPLOAD_FOLDER'] = 'uploads'
 @api.route('/')
 @api.route('/index/')
 def main_page():
@@ -37,6 +38,35 @@ def authenticate_login():
         return jsonify(response = "Success")
     return jsonify(response = 'ADfs')
 
+@api.route('/instructor/',methods = ["GET","POST"])
+def instructor_view():
+    message = "No file uploaded"
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filename = file.filename
+            path_of_file = "VOH/" + os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            print path_of_file
+            file.save(path_of_file)
+            message = "File has been uploaded!"
+        else:
+            message = "No file to upload!"
+    return render_template("instructor.html", message = message)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 @app.errorhandler(Exception)
 def exception_handler(error):
 
