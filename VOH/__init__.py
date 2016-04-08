f24309d22462219b3e309d0d3beeaf4ca4e179f6
Sudarshan Govindaprasad <Sudarshan@eduroamprvnat-172-21-96-212.near.illinois.edu>
2016-04-08 09:34:15 -0500

    changing starting address

--- VOH/__init__.py
+++ VOH/__init__.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 
 
 def create_app(debug=False):
-    """Create an application."""
     """
     @author : Sudarshan Govindprasad
     create_app creates an Flask app object, and a socket io object.
@@ -17,15 +16,18 @@ def create_app(debug=False):
     :param debug: debug Flag True or False
     """
     global app
+    print("HERE")
     app.config.from_object(__name__)
     app.config['UPLOAD_FOLDER'] = 'uploads'
     app.debug = debug
     app.config['SECRET_KEY'] = 'asdq2312ds1242!67#'
-
+    print("CONFIGURING APP")
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    print("CONFIGURING blueprint")
 
     socketio.init_app(app)
+    print("CONFIGURING SOCKETIO")
     return app
 
 
@@ -33,8 +35,8 @@ def open_db_connection():
     """
     Creates mongodb connection and returns client and current database instance
     """
-    # client = MongoClient('104.131.185.191', 27017)
-    client = MongoClient()
+    client = MongoClient('104.131.185.191', 27017)
+    # client = MongoClient()
     db = client["225VOH"]
     return client, db
 
--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from VOH import create_app, socketio
 app = create_app(debug=True) # Create an application
 
 if __name__ == '__main__':
-    socketio.run(app)  # Run app as a socketio instance
+    socketio.run(app, host = '0.0.0.0')  # Run app as a socketio instance
