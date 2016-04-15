563eab8dae2b2f818d03104af5814ae807f1491b
aadhyagupta <Aadhya@wirelessprvnat-172-17-187-163.near.illinois.edu>
2016-04-14 19:45:04 -0500

    updated id

--- VOH/static/js/ta_status.js
+++ VOH/static/js/ta_status.js
@@ -22,9 +22,15 @@ function get_ta_status(data) {
             var ta_net_id = data[i]["net_id"];
             var ta_status = data[i]["status"];
             var ta_name = data[i]['name'];
+            id_add = ta_net_id + "add";
+            id_remove = ta_net_id + "remove";
             path = "../static/data/img/" + ta_net_id + ".jpg";
-            html_data = html_data.concat('<div class = "row"></div><a class="btn-floating green" >Join</a>');
-            html_data = html_data.concat('<a class="btn-floating red" >Leave</a>');
+            html_data = html_data.concat('<div class = "row"></div><a class="btn-floating green" id = "');
+            html_data = html_data.concat(id_add);
+            html_data = html_data.concat('">Join</a>');
+            html_data = html_data.concat('<a class="btn-floating red"id = "');
+            html_data = html_data.concat(id_remove);
+            html_data = html_data.concat('">Leave</a>');
             html_data = html_data.concat('<div class = "chip large"><img src =');
             html_data = html_data.concat(path);
             html_data = html_data.concat('></img>');
